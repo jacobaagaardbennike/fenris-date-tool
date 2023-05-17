b76c82e32ec709863e1c382b7a9a8c3bad28591b
Jacob Ågård Bennike <jab@obton.com>
2023-05-17 11:39:52 +0200

    First version with fixes

--- src/core/locale_to_utc.service.ts
+++ src/core/locale_to_utc.service.ts
@@ -1,16 +1,14 @@
-import { convertDanishTimeToUtc } from "../timezone_rules/denmark.rule";
-import TimeZone from "./timeZone.type";
-
+import { convertDanishTimeToUtc } from '../timezone_rules/denmark.rule';
+import TimeZone from './timezone.type';
 
 const ConvertLocaleTimeToUtc = (inputString: string, timeZone: TimeZone): string => {
-
   switch (timeZone) {
     case 'Europe/Copenhagen': {
       return convertDanishTimeToUtc(inputString);
     }
     default:
-      throw new Error('unspported timeZone')
+      throw new Error('unspported timeZone');
   }
-}
+};
 
 export default ConvertLocaleTimeToUtc;
